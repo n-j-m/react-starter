b4d19aea36a5e360381548000a5b98187e86f726
n-j-m <nick65@gmail.com>
2015-02-22 20:32:47 -0600

    remove console logging.

--- app/main.jsx
+++ app/main.jsx
@@ -8,8 +8,6 @@ import Router from "react-router";
 import routes from "./routes";
 
 const content = document.getElementById("content");
-console.log("main");
 Router.run(routes, Router.HistoryLocation, (Handler) => {
-  console.log("handle");
   return React.render(<Handler/>, content);
 });
